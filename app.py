b3714953b8d9db98e0d9fdba6e817fff95466609
nashgao <nashgao1992@gmail.com>
2020-10-20 17:36:04 +1000

    code update since the repo has not been pushed to pypi

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask_jsonrpc import JSONRPC
-import urllib.parse
 import json
 
 import wikipedia
@@ -14,15 +13,28 @@ rpc = JSONRPC(app, '/', enable_web_browsable_api=True)
 @rpc.method('/api/wiki')
 def wiki(query: str) -> str:
     wikipedia.set_lang('en')
+    return json.dumps(
+        filterResult(
+            wikipedia.page(
+                wikipedia.search(query)[0]
+            )))
+
+
+@app.route('/geo')
+@rpc.method('/api/geo')
+def geo(lat: float, lon: float) -> str:
+    wikipedia.set_lang('en')
+
+    return json.dumps(
+        filterResult(
+            wikipedia.page(
+                wikipedia.geosearch(lat, lon))))
+
+
+def filterResult(result: wikipedia.WikipediaPage):
     resultContainer = {}
-
-    result = wikipedia.page(
-        urllib.parse.quote(
-            wikipedia.search(query)[0]
-        ))
-
     for attribute in dir(result):
-        if not attribute.startswith('_'):
+        if not attribute.startswith('-'):
             try:
                 classAttr = result.__getattribute__(attribute)
                 if isinstance(classAttr, str):
@@ -31,5 +43,5 @@ def wiki(query: str) -> str:
                 continue
             except AttributeError:
                 continue
+    return resultContainer
 
-    return json.dumps(resultContainer)
